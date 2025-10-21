5f6366b99e91018a207214f13802dfca8172662a
MortyTsai <mortyyuhau@gmail.com>
2025-10-21 16:14:56 +0800

    chore: 更新 renderers 套件的模組導出

--- src/projectinsight/renderers/__init__.py
+++ src/projectinsight/renderers/__init__.py
@@ -2,10 +2,9 @@
 """
 渲染器套件，負責將圖形資料結構視覺化為圖檔。
 """
-from .flow_renderer import render_flow_graph
-from .renderer import render_graph
+
+from .component_renderer import render_component_graph
 
 __all__ = [
-    "render_flow_graph",
-    "render_graph",
+    "render_component_graph",
 ]
