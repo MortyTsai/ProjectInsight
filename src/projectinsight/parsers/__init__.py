559202c9a0fb8143eea97a488163162544369244
MortyTsai <mortyyuhau@gmail.com>
2025-10-21 16:14:48 +0800

    chore: 更新 parsers 套件的模組導出

--- src/projectinsight/parsers/__init__.py
+++ src/projectinsight/parsers/__init__.py
@@ -2,9 +2,9 @@
 """
 解析器套件，提供將原始碼轉換為結構化資料的功能。
 """
-from . import flow_parser, py_parser
+
+from . import component_parser
 
 __all__ = [
-    "flow_parser",
-    "py_parser",
+    "component_parser",
 ]
