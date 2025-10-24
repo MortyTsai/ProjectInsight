78ce4cab0558a67fc7f49f4f30a10ff65d44525a
MortyTsai <mortyyuhau@gmail.com>
2025-10-24 19:03:53 +0800

    refactor(Builder): 重構 __init__.py 以匯出新模組

---
+++ build/lib/projectinsight/builders/__init__.py
@@ -0,0 +1,10 @@
+# src/projectinsight/builders/__init__.py
+"""
+建構器套件，負責將解析出的資料轉換為圖形資料結構。
+"""
+
+from .component_builder import build_component_graph_data
+
+__all__ = [
+    "build_component_graph_data",
+]
