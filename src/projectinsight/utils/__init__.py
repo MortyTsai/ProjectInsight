7cb5b3a785b3a236345527fcf89f15a44c13cb9d
MortyTsai <mortyyuhau@gmail.com>
2025-11-23 05:30:23 +0800

    refactor(Pkg): 更新工具套件介面以導出 parser_utils

--- src/projectinsight/utils/__init__.py
+++ src/projectinsight/utils/__init__.py
@@ -5,10 +5,14 @@
 
 from .file_system_utils import generate_tree_structure
 from .logging_utils import PickleFilter
+from .parser_utils import DECORATOR_IGNORE_PREFIXES, GLOBAL_IGNORE_PREFIXES, is_noise
 from .path_utils import find_project_root
 
 __all__ = [
+    "DECORATOR_IGNORE_PREFIXES",
+    "GLOBAL_IGNORE_PREFIXES",
     "PickleFilter",
     "find_project_root",
     "generate_tree_structure",
+    "is_noise",
 ]
